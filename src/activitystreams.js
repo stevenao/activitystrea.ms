4f567836420520354d6d5d6f27ce262df46123bd
James M Snell <jasnell@gmail.com>
2015-10-24 09:55:38 -0700

    Quick bug fix

--- src/activitystreams.js
+++ src/activitystreams.js
@@ -21,7 +21,7 @@ function _types(types, additional) {
 module.exports = {
   use(extension) {
     if (extension && typeof extension.init === 'function')
-      extension.init(this.models, reasoner, ext_context);
+      extension.init(module.exports.models, reasoner, ext_context);
   },
 
   get verify() {
@@ -45,243 +45,243 @@ module.exports = {
   },
 
   langmap() {
-    return new this.models.LanguageValue.Builder();
+    return new module.exports.models.LanguageValue.Builder();
   },
 
   object(types) {
-    return new this.models.Object.Builder(_types(types));
+    return new module.exports.models.Object.Builder(_types(types));
   },
 
   actor(types) {
-    return new this.models.Actor.Builder(_types(types));
+    return new module.exports.models.Actor.Builder(_types(types));
   },
 
   activity(types) {
-    return new this.models.Activity.Builder(_types(types));
+    return new module.exports.models.Activity.Builder(_types(types));
   },
 
   collection(types) {
-    return new this.models.Collection.Builder(_types(types));
+    return new module.exports.models.Collection.Builder(_types(types));
   },
 
   orderedCollection(types) {
-    return new this.models.OrderedCollection.Builder(_types(types));
+    return new module.exports.models.OrderedCollection.Builder(_types(types));
   },
 
   collectionPage(types) {
-    return new this.models.CollectionPage.Builder(_types(types));
+    return new module.exports.models.CollectionPage.Builder(_types(types));
   },
 
   orderedCollectionPage(types) {
-    return new this.models.OrderedCollectionPage.Builder(_types(types));
+    return new module.exports.models.OrderedCollectionPage.Builder(_types(types));
   },
 
   content(types) {
-    return new this.models.Content.Builder(_types(types));
+    return new module.exports.models.Content.Builder(_types(types));
   },
 
   link(types) {
-    return new this.models.Link.Builder(_types(types));
+    return new module.exports.models.Link.Builder(_types(types));
   },
 
   accept(types) {
-    return new this.models.Activity.Builder(_types(types,as.Accept));
+    return new module.exports.models.Activity.Builder(_types(types,as.Accept));
   },
 
   tentativeAccept(types) {
-    return new this.models.Activity.Builder(_types(types,as.TentativeAccept));
+    return new module.exports.models.Activity.Builder(_types(types,as.TentativeAccept));
   },
 
   add(types) {
-    return new this.models.Activity.Builder(_types(types,as.Add));
+    return new module.exports.models.Activity.Builder(_types(types,as.Add));
   },
 
   arrive(types) {
-    return new this.models.Activity.Builder(_types(types,as.Arrive));
+    return new module.exports.models.Activity.Builder(_types(types,as.Arrive));
   },
 
   create(types) {
-    return new this.models.Activity.Builder(_types(types,as.Create));
+    return new module.exports.models.Activity.Builder(_types(types,as.Create));
   },
 
   delete(types) {
-    return new this.models.Activity.Builder(_types(types,as.Delete));
+    return new module.exports.models.Activity.Builder(_types(types,as.Delete));
   },
 
   follow(types) {
-    return new this.models.Activity.Builder(_types(types,as.Follow));
+    return new module.exports.models.Activity.Builder(_types(types,as.Follow));
   },
 
   ignore(types) {
-    return new this.models.Activity.Builder(_types(types,as.Ignore));
+    return new module.exports.models.Activity.Builder(_types(types,as.Ignore));
   },
 
   join(types) {
-    return new this.models.Activity.Builder(_types(types,as.Join));
+    return new module.exports.models.Activity.Builder(_types(types,as.Join));
   },
 
   leave(types) {
-    return new this.models.Activity.Builder(_types(types,as.Leave));
+    return new module.exports.models.Activity.Builder(_types(types,as.Leave));
   },
 
   like(types) {
-    return new this.models.Activity.Builder(_types(types,as.Like));
+    return new module.exports.models.Activity.Builder(_types(types,as.Like));
   },
 
   offer(types) {
-    return new this.models.Activity.Builder(_types(types,as.Offer));
+    return new module.exports.models.Activity.Builder(_types(types,as.Offer));
   },
 
   invite(types) {
-    return new this.models.Activity.Builder(_types(types,as.Invite));
+    return new module.exports.models.Activity.Builder(_types(types,as.Invite));
   },
 
   reject(types) {
-    return new this.models.Activity.Builder(_types(types,as.Reject));
+    return new module.exports.models.Activity.Builder(_types(types,as.Reject));
   },
 
   tentativeReject(types) {
-    return new this.models.Activity.Builder(_types(types,as.TentativeReject));
+    return new module.exports.models.Activity.Builder(_types(types,as.TentativeReject));
   },
 
   remove(types) {
-    return new this.models.Activity.Builder(_types(types,as.Remove));
+    return new module.exports.models.Activity.Builder(_types(types,as.Remove));
   },
 
   undo(types) {
-    return new this.models.Activity.Builder(_types(types,as.Undo));
+    return new module.exports.models.Activity.Builder(_types(types,as.Undo));
   },
 
   update(types) {
-    return new this.models.Activity.Builder(_types(types,as.Update));
+    return new module.exports.models.Activity.Builder(_types(types,as.Update));
   },
 
   experience(types) {
-    return new this.models.Activity.Builder(_types(types,as.Experience));
+    return new module.exports.models.Activity.Builder(_types(types,as.Experience));
   },
 
   view(types) {
-    return new this.models.Activity.Builder(_types(types,as.View));
+    return new module.exports.models.Activity.Builder(_types(types,as.View));
   },
 
   listen(types) {
-    return new this.models.Activity.Builder(_types(types,as.Listen));
+    return new module.exports.models.Activity.Builder(_types(types,as.Listen));
   },
 
   read(types) {
-    return new this.models.Activity.Builder(_types(types,as.Read));
+    return new module.exports.models.Activity.Builder(_types(types,as.Read));
   },
 
   move(types) {
-    return new this.models.Activity.Builder(_types(types,as.Move));
+    return new module.exports.models.Activity.Builder(_types(types,as.Move));
   },
 
   travel(types) {
-    return new this.models.Activity.Builder(_types(types,as.Travel));
+    return new module.exports.models.Activity.Builder(_types(types,as.Travel));
   },
 
   announce(types) {
-    return new this.models.Activity.Builder(_types(types,as.Announce));
+    return new module.exports.models.Activity.Builder(_types(types,as.Announce));
   },
 
   block(types) {
-    return new this.models.Activity.Builder(_types(types,as.Block));
+    return new module.exports.models.Activity.Builder(_types(types,as.Block));
   },
 
   flag(types) {
-    return new this.models.Activity.Builder(_types(types,as.Flag));
+    return new module.exports.models.Activity.Builder(_types(types,as.Flag));
   },
 
   dislike(types) {
-    return new this.models.Activity.Builder(_types(types,as.Dislike));
+    return new module.exports.models.Activity.Builder(_types(types,as.Dislike));
   },
 
   application(types) {
-    return new this.models.Actor.Builder(_types(types,as.Application));
+    return new module.exports.models.Actor.Builder(_types(types,as.Application));
   },
 
   group(types) {
-    return new this.models.Actor.Builder(_types(types,as.Group));
+    return new module.exports.models.Actor.Builder(_types(types,as.Group));
   },
 
   person(types) {
-    return new this.models.Actor.Builder(_types(types,as.Person));
+    return new module.exports.models.Actor.Builder(_types(types,as.Person));
   },
 
   process(types) {
-    return new this.models.Actor.Builder(_types(types,as.Process));
+    return new module.exports.models.Actor.Builder(_types(types,as.Process));
   },
 
   service(types) {
-    return new this.models.Actor.Builder(_types(types,as.Service));
+    return new module.exports.models.Actor.Builder(_types(types,as.Service));
   },
 
   organization(types) {
-    return new this.models.Actor.Builder(_types(types,as.Organization));
+    return new module.exports.models.Actor.Builder(_types(types,as.Organization));
   },
 
   article(types) {
-    return new this.models.Content.Builder(_types(types,as.Article));
+    return new module.exports.models.Content.Builder(_types(types,as.Article));
   },
 
   album(types) {
-    return new this.models.Collection.Builder(_types(types,as.Album));
+    return new module.exports.models.Collection.Builder(_types(types,as.Album));
   },
 
   folder(types) {
-    return new this.models.Collection.Builder(_types(types,as.Folder));
+    return new module.exports.models.Collection.Builder(_types(types,as.Folder));
   },
 
   story(types) {
-    return new this.models.OrderedCollection.Builder(_types(types,as.Story));
+    return new module.exports.models.OrderedCollection.Builder(_types(types,as.Story));
   },
 
   document(types) {
-    return new this.models.Content.Builder(_types(types,as.Document));
+    return new module.exports.models.Content.Builder(_types(types,as.Document));
   },
 
   audio(types) {
-    return new this.models.Content.Builder(_types(types,as.Audio));
+    return new module.exports.models.Content.Builder(_types(types,as.Audio));
   },
 
   image(types) {
-    return new this.models.Content.Builder(_types(types,as.Image));
+    return new module.exports.models.Content.Builder(_types(types,as.Image));
   },
 
   video(types) {
-    return new this.models.Content.Builder(_types(types,as.Video));
+    return new module.exports.models.Content.Builder(_types(types,as.Video));
   },
 
   note(types) {
-    return new this.models.Content.Builder(_types(types,as.Note));
+    return new module.exports.models.Content.Builder(_types(types,as.Note));
   },
 
   page(types) {
-    return new this.models.Content.Builder(_types(types,as.Page));
+    return new module.exports.models.Content.Builder(_types(types,as.Page));
   },
 
   question(types) {
-    return new this.models.Question.Builder(_types(types));
+    return new module.exports.models.Question.Builder(_types(types));
   },
 
   event(types) {
-    return new this.models.Object.Builder(_types(types,as.Event));
+    return new module.exports.models.Object.Builder(_types(types,as.Event));
   },
 
   relationship(types) {
-    return new this.models.Relationship.Builder(_types(types));
+    return new module.exports.models.Relationship.Builder(_types(types));
   },
 
   profile(types) {
-    return new this.models.Profile.Builder(_types(types));
+    return new module.exports.models.Profile.Builder(_types(types));
   },
 
   place(types) {
-    return new this.models.Place.Builder(_types(types));
+    return new module.exports.models.Place.Builder(_types(types));
   },
 
   mention(types) {
-    return new this.models.Link.Builder(_types(types,as.Mention));
+    return new module.exports.models.Link.Builder(_types(types,as.Mention));
   },
 
   get interval() {
